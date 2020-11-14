560729407f141dab6af2873830b614ca274e6469
NamedRoger <roge.villa.carrera@hotmail.com>
2020-11-13 21:44:49 -0600

    init

---
+++ src/com/NamedRoger/Main.java
@@ -0,0 +1,10 @@
+package com.NamedRoger;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        AddressBook addressBook = new AddressBook();
+    }
+}
